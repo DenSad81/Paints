9f31b3e1866df14db52ad080793a941331c66b9c
DenSad81 <tagil81@mail.ru>
2024-04-14 10:09:00 +0300

    Добавьте файлы проекта.

---
+++ Paints/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args) {
+        int quantityPictureInRow = 3;
+        int quantityPictures = 52;
+        int quantityFoolRows;
+        int quantityOverPictures;
+
+        quantityFoolRows = quantityPictures / quantityPictureInRow;
+        quantityOverPictures = quantityPictures % quantityPictureInRow;
+        Console.WriteLine($"Количество полных рядов: {quantityFoolRows} остаток {quantityOverPictures}");
+    }
+}
